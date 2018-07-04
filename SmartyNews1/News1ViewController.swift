6147a06fe9deb83ebbe2c74ca5f83959d3e3e8c0
Takuma Osada <judgment9ttk069@gmail.com>
2018-07-04 18:08:46 +0900

    make parse and add some buttons

--- SmartyNews1/News1ViewController.swift
+++ SmartyNews1/News1ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class News1ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIWebViewDelegate {
+class News1ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIWebViewDelegate, XMLParserDelegate {
   
   var tableView: UITableView = UITableView()
   
@@ -17,6 +17,17 @@ class News1ViewController: UIViewController, UITableViewDelegate, UITableViewDat
   var webView: UIWebView = UIWebView()
   
   var goButton: UIButton!
+  var backButton: UIButton!
+  var cancelButton: UIButton!
+  
+  var dotsView: DotsLoader = DotsLoader()
+  
+  var parser = XMLParser()
+  var totalBox = NSMutableArray()
+  var elements = NSMutableDictionary()
+  var element = String()
+  var titleString = NSMutableArray()
+  var linkString = NSMutableArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,11 +66,60 @@ class News1ViewController: UIViewController, UITableViewDelegate, UITableViewDat
       goButton.setImage(UIImage(named: "go.png"), for: .normal)
       goButton.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
       self.view.addSubview(goButton)
+      
+      // button to access previous page
+      backButton = UIButton()
+      backButton.frame = CGRect(x: 10, y: self.view.frame.size.height - 128.0, width: 50, height: 50)
+      backButton.setImage(UIImage(named: "back.png"), for: .normal)
+      backButton.addTarget(self, action: #selector(backPage), for: .touchUpInside)
+      self.view.addSubview(backButton)
+      
+      // button to access cancel
+      cancelButton = UIButton()
+      cancelButton.frame = CGRect(x: 10, y: 80, width: 50, height: 50)
+      cancelButton.setImage(UIImage(named: "cancel.png"), for: .normal)
+      cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+      self.view.addSubview(cancelButton)
+      
+      goButton.isHidden = true
+      backButton.isHidden = true
+      cancelButton.isHidden = true
+      
+      //dots view
+      dotsView.frame = CGRect(x: 0, y: self.view.frame.size.height / 3, width: self.view.frame.size.width, height: 100)
+      dotsView.dotsCount = 5
+      dotsView.dotsRadius = 10
+      self.view.addSubview(dotsView)
+      
+      dotsView.isHidden = true
+      
+      // search for xml
+      let url: String = "http://news.yahoo.co.jp/pickup/domestic/rss.xml"
+      let urlToSend:URL = URL(string:url)!
+      parser = XMLParser(contentsOf: urlToSend)!
+      parser.delegate = self
+      parser.parse()
+      
+      tableView.reloadData()
     }
   
-  //button to acess the next page
-  
   @objc func refresh() {
+    
+    perform(#selector(delay), with: nil, afterDelay: 2.0)
+    
+  }
+  
+  @objc func delay() {
+    
+    // search for xml
+    let url: String = "http://news.yahoo.co.jp/pickup/domestic/rss.xml"
+    let urlToSend:URL = URL(string:url)!
+    parser = XMLParser(contentsOf: urlToSend)!
+    parser.delegate = self
+    parser.parse()
+    
+    tableView.reloadData()
+    refreshControl.endRefreshing()
     
   }
   
@@ -67,6 +127,39 @@ class News1ViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     
   }
+  
+  @objc func backPage() {
+    
+    
+  }
+  
+  @objc func cancel() {
+    
+    
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+    return 100
+    
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    
+    return 1
+    
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    return totalBox.count
+
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    <#code#>
+  }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
